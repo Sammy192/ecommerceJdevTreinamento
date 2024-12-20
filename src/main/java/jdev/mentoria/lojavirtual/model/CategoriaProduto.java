15fa80dc68d5cae6ddc080c4485cbb91256f6bac
Samuel Rodrigues Borges <samuel.borges@e-unicred.com.br>
2024-12-20 11:39:58 -0300

    Cria classe CategoriaProduto

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -0,0 +1,40 @@
+package jdev.mentoria.lojavirtual.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categoria_produto")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+    private Long id;
+    @Column(nullable = false)
+    private String nomeDesc;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CategoriaProduto)) return false;
+        CategoriaProduto that = (CategoriaProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
