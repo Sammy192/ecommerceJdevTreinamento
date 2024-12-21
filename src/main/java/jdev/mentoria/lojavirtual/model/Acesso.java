e4937fc9524f1c90021b7ca51dd5ccd0744a15c7
Samuel Rodrigues Borges <samuel.borges@e-unicred.com.br>
2024-12-20 21:48:15 -0300

    Cria classe Acesso

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
@@ -0,0 +1,42 @@
+package jdev.mentoria.lojavirtual.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "acesso")
+@Getter
+@Setter
+@SequenceGenerator(name ="seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+
+    @Column(nullable = false)
+    private String perfil;
+    @Override
+    public String getAuthority() {
+        return this.perfil;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Acesso acesso = (Acesso) o;
+        return Objects.equals(id, acesso.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
