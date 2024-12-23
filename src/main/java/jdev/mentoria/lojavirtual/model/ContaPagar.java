f438cc167932c218f71ca9575a2d30ef0aa7915b
Samuel Rodrigues Borges <samuel.borges@e-unicred.com.br>
2024-12-23 10:30:11 -0300

    Cria classes ContaReceber, ContaPagar, FormaPagamento e Enum

--- src/main/java/jdev/mentoria/lojavirtual/model/ContaPagar.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/ContaPagar.java
@@ -1,7 +1,7 @@
 package jdev.mentoria.lojavirtual.model;
 
+import jdev.mentoria.lojavirtual.enums.StatusContaPagar;
 import jdev.mentoria.lojavirtual.enums.StatusContaReceber;
-import jdev.mentoria.lojavirtual.enums.TipoEndereco;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,38 +14,43 @@ import java.util.Date;
 import java.util.Objects;
 
 @Entity
-@Table(name = "conta_receber")
+@Table(name = "conta_pagar")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@SequenceGenerator(name = "seq_conta_receber", sequenceName = "seq_conta_receber", allocationSize = 1, initialValue = 1)
-public class ContaReceber implements Serializable {
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_receber")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
     private Long id;
 
     private String descricao;
+    private BigDecimal valorTotal;
+    private BigDecimal valorDesconto;
+
     @Enumerated(EnumType.STRING)
-    private StatusContaReceber statusContaReceber;
+    private StatusContaPagar statusContaPagar;
     @Temporal(TemporalType.DATE)
     private Date dataVencimento;
     @Temporal(TemporalType.DATE)
     private Date dataPagamento;
-    private BigDecimal valorTotal;
-    private BigDecimal valorDesconto;
 
     @ManyToOne(targetEntity = Pessoa.class)
-    @JoinColumn(name = "pessoa_id", nullable = false , foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_contaReceber_pessoa"))
+    @JoinColumn(name = "pessoa_id", nullable = false , foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_contaPagar_pessoa"))
     private Pessoa pessoa;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoaFornecedor_id", nullable = false , foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "fk_contaPagar_pessoaFornecedor"))
+    private Pessoa pessoaFornecedor;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ContaReceber that = (ContaReceber) o;
+        if (!(o instanceof ContaPagar)) return false;
+        ContaPagar that = (ContaPagar) o;
         return Objects.equals(id, that.id);
     }
 
---
+++ src/main/java/jdev/mentoria/lojavirtual/model/FormaPagamento.java
@@ -0,0 +1,38 @@
+package jdev.mentoria.lojavirtual.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "forma_pagamento")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SequenceGenerator(name = "seq_forma_pagamento", sequenceName = "seq_forma_pagamento", allocationSize = 1, initialValue = 1)
+public class FormaPagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_forma_pagamento")
+    private Long id;
+
+    private String descricao;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FormaPagamento)) return false;
+        FormaPagamento that = (FormaPagamento) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
